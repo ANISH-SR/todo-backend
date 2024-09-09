5db53918c50490391a9102c8a6cc2d7d49f84b02
Anish Singh Rawat <rawatanish08@gmail.com>
2024-09-09 16:37:55 +0530

    Modified index2.js

--- index2.js
+++ index2.js
@@ -1,8 +1,31 @@
 //Assignment #2 - Trying to code a filesystem based todo app and store data into the file
 
+const { rejects } = require("assert");
 const express = require("express");
+const fs = require("fs");
 const app = express();
 
+
+
 app.post("/addtodo", (req, res) => {
+	return new Promise((resolve, reject) => {
+		fs.readFile("a.txt", "utf-8", (err, data) => {
+			if (!err) {
+				resolve(data);
+			}
+			else {
+				reject(err);
+			}
+		})
+
+	}).then()
+
+})
+
+app.delete("/deletetodo", (req, res) => {
+
+})
+
+app.get("/gettodo", (req, res) => {
 
 })
