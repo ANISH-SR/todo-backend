c263a2cbfd385e74b020446d7177a0b39727ec51
Anish Singh Rawat <rawatanish08@gmail.com>
2024-09-10 15:26:23 +0530

    Added endpoints to index2.js

--- index2.js
+++ index2.js
@@ -1,6 +1,5 @@
 //Assignment #2 - Trying to code a filesystem based todo app and store data into the file
 
-const { rejects } = require("assert");
 const express = require("express");
 const fs = require("fs");
 const app = express();
@@ -8,17 +7,7 @@ const app = express();
 
 
 app.post("/addtodo", (req, res) => {
-	return new Promise((resolve, reject) => {
-		fs.readFile("a.txt", "utf-8", (err, data) => {
-			if (!err) {
-				resolve(data);
-			}
-			else {
-				reject(err);
-			}
-		})
 
-	}).then()
 
 })
 
